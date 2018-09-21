57d1cd3f226fcb3815b3ca97ca933bb5a59bf603
Rian Finnegan <flanagan89@gmail.com>
2018-09-21 11:18:45 +1000

    adding stability testing modules and deployments

---
+++ StabilityTester/modules/EventParser/app.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var Transport = require('azure-iot-device-mqtt').Mqtt;
+var Client = require('azure-iot-device').ModuleClient;
+var Message = require('azure-iot-device').Message;
+
+Client.fromEnvironment(Transport, function (err, client) {
+  if (err) {
+    throw err;
+  } else {
+    client.on('error', function (err) {
+      throw err;
+    });
+
+    // connect to the Edge instance
+    client.open(function (err) {
+      if (err) {
+        throw err;
+      } else {
+        console.log('IoT Hub module client initialized');
+
+        // Act on input messages to the module.
+        client.on('inputMessage', function (inputName, msg) {
+          pipeMessage(client, inputName, msg);
+        });
+      }
+    });
+  }
+});
+var count = 0;
+// This function just pipes the messages without any change.
+function pipeMessage(client, inputName, msg) {
+  client.complete(msg, printResultFor('Receiving message'));
+
+  if (inputName === 'input1' || inputName === 'input2') {
+    var message = msg.getBytes().toString('utf8');
+    var data = JSON.parse(message);
+    data.messageNumber = count++;
+    message = JSON.stringify(data);
+    if (message) {
+      var outputMsg = new Message(message);
+      client.sendOutputEvent('output1', outputMsg, printResultFor('Sending received message'));
+    }
+  } else {
+    console.log(`Unknown Input: ${inputName}`);
+  }
+}
+
+// Helper function to print results in the console
+function printResultFor(op) {
+  return function printResult(err, res) {
+    if (err) {
+      console.log(op + ' error: ' + err.toString());
+    }
+    if (res) {
+      console.log(op + ' status: ' + res.constructor.name);
+    }
+  };
+}
